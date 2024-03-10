c0c8dc462160a04f79e9fc6c83d87f613e09d1a1
Artem Nikolaichuk <antyartem1@yandex.ru>
2024-03-11 02:18:11 +0300

    Add gameplay function

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,2 +1,17 @@
+import random
+
+
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
     return sum(map(lambda x: x[0] == x[1], zip(guess, secret))), sum(map(lambda x: x in secret, set(guess)))
+
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = random.choices(words)
+    guessed = None
+    asked = 0
+    while guessed != secret:
+        guessed = ask("Введите слово: ", words)
+        asked += 1
+        b, c = bullscows(guessed, secret)
+        inform("Быки: {}, Коровы: {}", b, c)
+    return asked
